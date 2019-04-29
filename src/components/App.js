1ed111544f9da12611183a3d3c7c41401ee57d01
Eric Hunzeker <emh128@pitt.edu>
2019-04-28 23:11:18 -0400

    start homepage

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <table id="streaming-options">
+          <th id="spotify-link">spotify</th>
+          <th> / </th>
+          <th id="apple-music-link">apple music</th>
+        </table>
+      </header>
+    </div>
+  );
+}
+
+export default App;
