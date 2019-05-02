9094fa3630b186453cb2992964cf7f9abf87ae0e
Eric Hunzeker <emh128@pitt.edu>
2019-05-02 08:13:26 -0400

    figured out project flow finally
    need reducer for spotify token

--- src/components/App.js
+++ src/components/App.js
@@ -3,31 +3,16 @@ import './App.css';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import SpotifyLogin from './spotify/SpotifyLogin.js';
 import * as SpotifyFunctions from './spotify/SpotifyFunctions';
-
+import HomePage from './HomePage';
 
 class App extends Component{
 
     render() {
         return (
-            <Router>
                 <div className="App">
-                    <header className="App-header">
-                        <table id="streaming-options">
-                            <th id="spotify-link">
-                                <SpotifyLogin/>
-                            </th>
-                            <th id="apple-music-link">apple music</th>
-                        </table>
-                        <p id="app-description">login to either platform and monthly will automatically create 12 playlists
-                            - each containing every song you've
-                            ever saved or added to a playlist, organized by month</p>
-
-                        <Route path="/spotify/" component={SpotifyLogin}/>
-
-                    </header>
+                    <HomePage/>
                 </div>
 
-            </Router>
         );
     }
 }
